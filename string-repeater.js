94fbf1bdae1cac482fc1559f286163961d7e2839
Aaron Russell Jr <adrusselljr@Aarons-MacBook-Pro.local>
2021-09-27 15:56:19 -0400

    edit js

--- string-repeater.js
+++ string-repeater.js
@@ -1,8 +1,9 @@
 const prompt = require("prompt-sync")({ sigint:true })
 
 let string = prompt("Enter A String: ")
+let string1= string
 
-while(string.length <= 100) {
-    string += string
+while(string.length <= 10) {
+    string += string1
     console.log(string)
 }
